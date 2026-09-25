63dd27d60a16760c31e7fc4b56a833850eae090c
Abhishekkushwaha15 <kushwahaabhi@outlook.in>
2026-09-25 23:04:39 +0530

    update failed test

--- app/app/settings.py
+++ app/app/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgrasql',
+        'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS '),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 

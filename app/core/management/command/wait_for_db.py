866d6dba922ad9953be4b2f6a6b317b35e4cf7c8
Abhishekkushwaha15 <kushwahaabhi@outlook.in>
2026-09-26 22:19:00 +0530

    django wait for database

---
+++ app/core/management/command/wait_for_db.py
@@ -0,0 +1,9 @@
+"""
+django command to wiat for database
+"""
+from django.core.management.base import BaseCommand  
+
+class Command(BaseCommand):
+
+    def Handle(self, *args, **options):
+        pass 
